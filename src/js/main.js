12788ef2230e52ca810761af2dc710bda1b8a37c
Matt Daniels <matthew.f.daniels@gmail.com>
2020-09-08 12:51:36 -0400

    here

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,6 @@ import header from "./header";
 const $body = d3.select("body");
 let previousWidth = 0;
 
-function resize() {
-  // only do resize on width changes, not height
-  // (remove the conditional if you want to trigger on height change)
-  const width = $body.node().offsetWidth;
-  if (previousWidth !== width) {
-    previousWidth = width;
-    graphic.resize();
-  }
-}
-
 function setupStickyHeader() {
   const $header = $body.select("header");
   if ($header.classed("is-sticky")) {
@@ -39,12 +29,10 @@ function init() {
   header.init();
   // add mobile class to body tag
   $body.classed("is-mobile", isMobile.any());
-  // setup resize event
-  window.addEventListener("resize", debounce(resize, 150));
   // setup sticky header menu
   // setupStickyHeader();
   // kick off graphic code
-  // graphic.init();
+  graphic.init();
   // load footer stories
   footer.init();
 }
