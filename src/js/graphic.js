5612f4ff5d173780108fdf0467a8e66f403b7103
Matt Daniels <matthew.f.daniels@gmail.com>
2020-09-08 15:26:20 -0400

    updates

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -18,15 +18,21 @@ let currentlyPlayingSong;
 function setupHowlerList() {
 
   const inTextSamples = [{
-      artist_song: 'no-diggity',
-      song_url: `https://p.scdn.co/mp3-preview/d887520d295cec271b8396b4c131f5f55348c3a3`
+      artist_song: 'thong',
+      song_url: `https://p.scdn.co/mp3-preview/c4b86b4adfe16bfbdad84d1dfbbec5a18ca297ed`
     },
     {
-      artist_song: 'the-sign',
-      song_url: 'https://p.scdn.co/mp3-preview/304efabb42448c99202a7659e43b502a6324d981'
+      artist_song: 'eternal',
+      song_url: 'https://p.scdn.co/mp3-preview/c558bc461278efa0159e732a1eb8e314ed1758f9'
     }, {
       artist_song: 'cold',
       song_url: 'https://p.scdn.co/mp3-preview/0115e602f170e642fe3c1259650ab38fd3c8a0fc'
+    }, {
+      artist_song: 'stay',
+      song_url: 'https://p.scdn.co/mp3-preview/395ea5f63e7c0375529493a5f4bf5c58a8ff73f0'
+    }, {
+      artist_song: 'reason',
+      song_url: 'https://p.scdn.co/mp3-preview/b88392e350a215b5f6f4cf309b6ceabe925ec1a8'  
     }, {
       artist_song: 'smack',
       song_url: 'https://p.scdn.co/mp3-preview/0ac099ae429cd14e7424bb1d8921a02fc1b3ceeb'
@@ -53,6 +59,60 @@ function setupHowlerList() {
 
 
 function setupHowlerPlayback() {
+
+  function vennPlay(howlSong){
+
+    Object.keys(howlerList).forEach(key => howlerList[key].stop());
+
+    if (howlSong === currentlyPlayingSong) {
+      howlerList[howlSong].stop()
+      currentlyPlayingSong = null
+      return
+    }
+
+    howlerList[howlSong].once('load', function () {
+      howlerList[howlSong].fade(0, .5, 2000);
+      howlerList[howlSong].play();
+      currentlyPlayingSong = howlSong
+    });
+    howlerList[howlSong].load()
+  }
+
+  d3.selectAll('#cold-hearted').on('click', function(d,i,n) {
+    const howlSong = "cold"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#reason').on('click', function(d,i,n) {
+    const howlSong = "reason"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#stay').on('click', function(d,i,n) {
+    const howlSong = "stay"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#stayin').on('click', function(d,i,n) {
+    const howlSong = "stayin"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#smack').on('click', function(d,i,n) {
+    const howlSong = "smack"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#eternal-flame').on('click', function(d,i,n) {
+    const howlSong = "eternal"
+    vennPlay(howlSong)
+  });
+
+  d3.selectAll('#thong-song').on('click', function(d,i,n) {
+    const howlSong = "thong"
+    vennPlay(howlSong)
+  });
+
 
   d3.selectAll('.howler-icon').on('click', function(d,i,n) {
 
